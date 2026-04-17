ac197a39b72dfc0bb29c53928ffb781e6db4b7db
Nicolas Vero <nicolasvero03@gmail.com>
2026-04-18 01:04:41 +0200

    Implement sorted image

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ def sort_pixels(image_path: str, output_path: str):
     image = Image.open(image_path).convert("RGB")
     pixels = np.array(image)
 
-    for i in range(pixels.shape[0]):
-        for j in range(pixels.shape[1]):
-            print(pixels[i , j])
+    flat = pixels.reshape(-1, 3).astype(np.int32)
+    hex_values = flat[:,0]*65536 + flat[:,1]*256 + flat[:,2]
+    indices = np.argsort(hex_values)
+
+    sorted_pixels = flat[indices]
+    h, w, _ = image.size[1], image.size[0], 3
+    sorted_pixels = sorted_pixels.reshape(h, w, 3)
+
+    result = Image.fromarray(sorted_pixels.astype(np.uint8))
+    result.save(output_path)   
 
 if __name__ == "__main__":
     sort_pixels("input.jpg", "output.jpg")
